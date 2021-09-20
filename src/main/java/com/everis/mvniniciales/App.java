68aef234e10c31b7337f17619afcab935c7d1ba1
zhongyi <zhongyichen255@gmail.com>
2021-09-20 14:00:03 +0200

    nombre

---
+++ src/main/java/com/everis/mvniniciales/App.java
@@ -0,0 +1,21 @@
+package com.everis.mvniniciales;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(capitalize("zhongyi Chen!")  );
+    }
+    
+    public static String capitalize(String str) {
+    	return StringUtils.capitalize(str);
+    }
+}
+
+
