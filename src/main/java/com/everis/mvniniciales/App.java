3e4f1e8440d6b324dc8beeeb96410e0a6002e611
zhongyi <zhongyichen255@gmail.com>
2021-09-20 14:04:28 +0200

    saludar con nombre

--- src/main/java/com/everis/mvniniciales/App.java
+++ src/main/java/com/everis/mvniniciales/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(capitalize("zhongyi Chen!")  );
+        System.out.println(capitalize("hola Zhongyi Chen!")  );
     }
     
     public static String capitalize(String str) {
